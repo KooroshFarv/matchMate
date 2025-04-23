ffb511fdcaaba3fe1b600daf8b0210b535648d31
kooroshFarv <farvardin.koorosh@gmail.com>
2025-04-22 20:02:00 -0400

    Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu"
+
+
+const Navbar = () => {
+    return ( 
+       <nav className="flex justify-between shadow-sm items-center p-4 ">
+        <h1>MatchMade</h1>
+        <div className="flex gap-10 justify-center">
+            <DropdownMenu>
+                <DropdownMenuTrigger className="cursor-pointer font-medium">
+                    Concepts
+                </DropdownMenuTrigger> 
+                <DropdownMenuContent>
+                    <DropdownMenuItem>Modern</DropdownMenuItem>
+                    <DropdownMenuItem>Minimal</DropdownMenuItem>
+                    <DropdownMenuItem>Cozy</DropdownMenuItem>
+                    <DropdownMenuItem>Industrial</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <div className="cursor-pointer font-medium hover:text-gray-800"> Inspiration </div>
+            <div className="cursor-pointer font-medium hover:text-gray-800"> Contact </div>
+        </div>
+            <Button variant='ghost' className="cursor-pointer">Sign In</Button>
+       </nav>
+     );
+}
+ 
+export default Navbar;
