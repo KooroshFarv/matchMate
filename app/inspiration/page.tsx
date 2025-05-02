0151839c92dc2c162b95023c10e4ecce6ca1a8c0
kooroshFarv <farvardin.koorosh@gmail.com>
2025-05-02 00:46:46 -0400

    Masonry

--- app/inspiration/page.tsx
+++ app/inspiration/page.tsx
@@ -1,16 +1,29 @@
+'use client'
 import {inspirationImages } from "../data/images";
+import Masonry from 'react-masonry-css'
+
+const breakPoint = {
+    default : 4,
+    1100 : 3,
+    700 : 2,
+    500 : 1,
+}
 
 const InspirationPage = () => {
     return ( 
-        <>
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-3">
-            {inspirationImages.map((url, index) => (
+        <div className="px-4 py-10">
+            < Masonry breakpointCols={breakPoint}
+            className="flex gap-4"
+            columnClassName="space-y-4 bg-clip-padding"
+            >
+                  {inspirationImages.map((url, index) => (
                 <img src={url} alt={`inspiration ${index}`} key={index}
                 className="w-full h-auto shadow-md object-cover rounded-lg"
                 />
             ))}
+            </Masonry>
         </div>
-        </>
+ 
      );
 }
  
